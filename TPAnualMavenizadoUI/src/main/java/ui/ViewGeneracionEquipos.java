ab83fb47bcd088cf2c5e16e325726570f7288d50
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-09-25 10:49:49 -0300

    Comentarios sobre la funcionalidad

--- TPAnualMavenizadoUI/src/main/java/ui/ViewGeneracionEquipos.java
+++ TPAnualMavenizadoUI/src/main/java/ui/ViewGeneracionEquipos.java
@@ -163,6 +163,67 @@ import org.uqbar.arena.windows.*;
 import org.uqbar.lacar.ui.model.*;
 import org.uqbar.lacar.ui.model.bindings.*;
 
+
+/*
+FIXME me genera este error cuando presiono el botón generar equipos, con división por pares
+y ordenando por calificaiciones. Además, no genera nada y muestra un diálogo de error. 
+
+
+ java.lang.ArrayIndexOutOfBoundsException: -1
+	at java.util.ArrayList.elementData(ArrayList.java:403)
+	at java.util.ArrayList.get(ArrayList.java:416)
+	at opf5.criteriosDeOrdenamientoDeEquipos.PromedioDeUltimoPartido.obtenerListaDeCriticasUltimoPartido(PromedioDeUltimoPartido.java:35)
+	at opf5.criteriosDeOrdenamientoDeEquipos.PromedioDeUltimoPartido.ponderate(PromedioDeUltimoPartido.java:22)
+	at opf5.estadoPartido.NoConfirmado.lambda$1(NoConfirmado.java:40)
+	at opf5.estadoPartido.NoConfirmado$$Lambda$11/1505964934.apply(Unknown Source)
+	at java.util.Comparator.lambda$comparing$77a9974f$1(Comparator.java:469)
+	at java.util.Comparator$$Lambda$12/1906128360.compare(Unknown Source)
+	at java.util.TimSort.countRunAndMakeAscending(TimSort.java:351)
+	at java.util.TimSort.sort(TimSort.java:216)
+	at java.util.Arrays.sort(Arrays.java:1507)
+	at java.util.stream.SortedOps$SizedRefSortingSink.end(SortedOps.java:302)
+	at java.util.stream.AbstractPipeline.copyInto(AbstractPipeline.java:513)
+	at java.util.stream.AbstractPipeline.wrapAndCopyInto(AbstractPipeline.java:502)
+	at java.util.stream.ReduceOps$ReduceOp.evaluateSequential(ReduceOps.java:708)
+	at java.util.stream.AbstractPipeline.evaluate(AbstractPipeline.java:234)
+	at java.util.stream.ReferencePipeline.collect(ReferencePipeline.java:499)
+	at opf5.estadoPartido.NoConfirmado.armarEquipos(NoConfirmado.java:41)
+	at opf5.Partido.armarEquipos(Partido.java:149)
+	at ui.UnViewModelGeneracion.generacionEquipos(UnViewModelGeneracion.java:107)
+	at ui.ViewGeneracionEquipos.lambda$0(ViewGeneracionEquipos.java:223)
+	at ui.ViewGeneracionEquipos$$Lambda$8/1217741734.execute(Unknown Source)
+	at org.uqbar.lacar.ui.impl.jface.actions.JFaceActionAdapter.widgetSelected(JFaceActionAdapter.java:42)
+	at org.eclipse.swt.widgets.TypedListener.handleEvent(TypedListener.java:228)
+	at org.eclipse.swt.widgets.EventTable.sendEvent(EventTable.java:84)
+	at org.eclipse.swt.widgets.Widget.sendEvent(Widget.java:1176)
+	at org.eclipse.swt.widgets.Display.runDeferredEvents(Display.java:3493)
+	at org.eclipse.swt.widgets.Display.readAndDispatch(Display.java:3112)
+	at org.eclipse.jface.window.Window.runEventLoop(Window.java:825)
+	at org.eclipse.jface.window.Window.open(Window.java:801)
+	at org.uqbar.lacar.ui.impl.jface.windows.JFaceWindowBuilder.open(JFaceWindowBuilder.java:110)
+	at org.uqbar.arena.windows.Window.open(Window.java:147)
+	at ui.ViewPpal.lambda$0(ViewPpal.java:19)
+	at ui.ViewPpal$$Lambda$6/404648734.execute(Unknown Source)
+	at org.uqbar.lacar.ui.impl.jface.actions.JFaceActionAdapter.widgetSelected(JFaceActionAdapter.java:42)
+	at org.eclipse.swt.widgets.TypedListener.handleEvent(TypedListener.java:228)
+	at org.eclipse.swt.widgets.EventTable.sendEvent(EventTable.java:84)
+	at org.eclipse.swt.widgets.Widget.sendEvent(Widget.java:1176)
+	at org.eclipse.swt.widgets.Display.runDeferredEvents(Display.java:3493)
+	at org.eclipse.swt.widgets.Display.readAndDispatch(Display.java:3112)
+	at org.eclipse.jface.window.Window.runEventLoop(Window.java:825)
+	at org.eclipse.jface.window.Window.open(Window.java:801)
+	at org.uqbar.lacar.ui.impl.jface.windows.JFaceWindowBuilder.open(JFaceWindowBuilder.java:110)
+	at org.uqbar.arena.windows.Window.open(Window.java:147)
+	at org.uqbar.arena.Application.run(Application.java:74)
+	at org.eclipse.core.databinding.observable.Realm.runWithDefault(Realm.java:288)
+	at org.uqbar.lacar.ui.impl.jface.JFaceApplicationBuilder.run(JFaceApplicationBuilder.java:25)
+	at org.uqbar.arena.Application.start(Application.java:55)
+	at org.uqbar.arena.windows.MainWindow.startApplication(MainWindow.java:30)
+	at ui.ViewPpal.main(ViewPpal.java:25)
+  
+  
+  
+ */
 public class ViewGeneracionEquipos extends SimpleWindow<UnViewModelGeneracion> {
 
 	public ViewGeneracionEquipos(WindowOwner parent) {
@@ -222,6 +283,10 @@ public class ViewGeneracionEquipos extends SimpleWindow<UnViewModelGeneracion> {
 	@Override
 	protected void addActions(Panel actionsPanel) {
 		new Button(actionsPanel).setCaption("GENERAR").onClick(() -> this.getModelObject().generacionEquipos());
+		//FIXME esto parece estar funcionando, pero 
+		//1. No hay ningún feedback visual de que la acción se haya ejecutado. Le doy click y no sé si funcionó o no
+		//2. Si confirmo dos veces un partido debería fallar e informar que ya está cofirmado, o al menos, indicar visualizar de alguna forma
+		//que ya lo está, así el usuario es consciente de que va a confirmar un partido ya confirmado. 
 		new Button(actionsPanel).setCaption("CONFIRMAR").onClick(()-> this.getModelObject().confirmarEquipos());
 		
 		Button verDatos = new Button(actionsPanel);
